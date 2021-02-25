02246587e12b6f74f93d037984fddcdbe0812f0e
LucasHCDev <lucas.h.cahill@gmail.com>
2021-02-24 18:25:24 -0800

    More Spring

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
+
+/*
+import ReactDOM from 'react-dom'; */
 import './App.css';
 import {useSpring, animated} from 'react-spring';
 
 
 function App() {
 
+const [state, toggle] = useState(true)
+const { x } = useSpring({
+  from: { x: 0 },
+  x: state ? 1 : 0,
+  config: {duration: 1000}   
+})
+
+return (
+  <div onClick={() => toggle(!state)}>
+    <animated.div
+    style={{
+      opacity: x.interpolate({ range: [0, 1], output: [0.3, 1] }),
+      transform: x
+      .interpolate({
+        range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+        output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1]
+      })
+      .interpolate(x => `scale(${x})`)
+    }}>
+      click
+    </animated.div>
+  </div>
+)
+/*
 const props = useSpring({
   to: async (next, cancel) => {
     await next({opacity: 1, color: '#ffaaee'})
@@ -19,7 +46,8 @@ const props = useSpring({
   <animated.div style={props}>
     I will fade in and out
   </animated.div>
-  </div>
+  </div> */
+
   /* return (
     <div className="App">
       <h1>My First React App
@@ -28,6 +56,6 @@ const props = useSpring({
     </div>
 
   ); */
-}
+  }
 
 export default App;

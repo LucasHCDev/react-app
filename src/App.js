875a1ab4dcd6570d8910e0cddd83aaf01a7a473d
LucasHCDev <lucas.h.cahill@gmail.com>
2021-02-24 17:10:24 -0800

    Testing spring hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useSpring, animated} from 'react-spring';
+
 
 function App() {
-  return (
+
+const props = useSpring({
+  to: async (next, cancel) => {
+    await next({opacity: 1, color: '#ffaaee'})
+    await next({opacity: 0, color: 'rbg(14, 26, 19'})
+    await next({opacity: 1, color: 'light-blue'})
+  },
+  from: {opacity: 0, color: 'red'}
+})
+  return <div className="App"> 
+  <h1>My React App</h1>
+  
+  <animated.div style={props}>
+    I will fade in and out
+  </animated.div>
+  </div>
+  /* return (
     <div className="App">
       <h1>My First React App
       </h1>
-      <p>a few more changes</p>
-      <p>even more changes</p>
+      <p>Using spring</p>
     </div>
-  );
+
+  ); */
 }
 
 export default App;
